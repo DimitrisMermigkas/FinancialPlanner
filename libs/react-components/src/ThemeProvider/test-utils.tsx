a08b42283de2eab69b3d6eb14f46dc8a910e73d0
DimitrisMermigkas <dmermigas@product-me.eu>
2024-11-24 23:20:07 +0200

    added queryClient to test-utils

--- libs/react-components/src/ThemeProvider/test-utils.tsx
+++ libs/react-components/src/ThemeProvider/test-utils.tsx
@@ -5,16 +5,34 @@ import { ThemeProvider, createTheme } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { DialogCtxProvider } from '../Dialogs/DialogCtxProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { DefaultOptions, QueryClient } from '@tanstack/react-query';
 
 // Import your custom theme if you have one, or use the default theme
 const theme = createTheme();
+
+const defaultOptionsConfig: DefaultOptions = {
+  queries: {
+    retry: false,
+    staleTime: 5 * 60 * 1000,
+    refetchOnWindowFocus: false,
+  },
+};
+
+export const queryClient = new QueryClient({
+  defaultOptions: defaultOptionsConfig,
+});
 
 const AllProviders: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => (
   <ThemeProvider theme={theme}>
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DialogCtxProvider>{children}</DialogCtxProvider>
+      <DialogCtxProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </DialogCtxProvider>
     </LocalizationProvider>
   </ThemeProvider>
 );
